b2957a244b30129f22479c749c700c1d8b810296
Nyamador <dselasi4u@gmail.com>
2020-08-16 01:08:48 +0000

    Added ResourceParser as subclass of Parser

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,9 +1,10 @@
 from typing import List
 from pathlib import Path
+import shutil
 
 
 class Parser:
-    extensions = [] #type: List[str]
+    extensions: List[str] = []
 
     def valid_extension(self, extension):
         return extension in self.extensions
@@ -19,3 +20,13 @@ class Parser:
         full_path = dest / path.with_suffix(ext).name
         with open(full_path, 'w') as file:
             file.write(content)
+
+    def copy(self, path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    extensions = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path, source, dest):
+        self.copy(path, source, dest)
