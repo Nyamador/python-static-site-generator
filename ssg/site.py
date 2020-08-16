b1c60ede59bd102f0d9c3c8ba9436d82c54ee8ff
Nyamador <dselasi4u@gmail.com>
2020-08-16 01:13:47 +0000

    Added load_parser method to Site class

--- ssg/site.py
+++ ssg/site.py
@@ -1,13 +1,19 @@
 from pathlib import Path
 
 class Site:
-    def __init__(self, source, dest):
+    def __init__(self, source, dest, parsers = None):
         self.source = Path(source)
         self.dest = Path(dest)
+        self.parsers = parses or []
 
     def create_dir(self, path):
         directory = self.dest/path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
+    
+    def load_parser(self, extension):
+        for parser in self.parsers:
+            if parser.valid_extension(extension):
+                return parser
 
     def build(self):
         self.dest.mkdir(parents=True, exist_ok=True)
