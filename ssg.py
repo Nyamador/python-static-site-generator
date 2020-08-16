b95e691cb98d296d84d8424ac730ea9b51ac0075
Nyamador <dselasi4u@gmail.com>
2020-08-16 01:25:33 +0000

    Fixed typos in parsers.py

--- ssg.py
+++ ssg.py
@@ -1,12 +1,12 @@
 import typer
 from ssg.site import  Site
-from ssg import parsers
+import ssg.parsers
 
 def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest,
-        "parsers": parsers.ResourceParser()
+        "parsers": [ssg.parsers.ResourceParser(),],
     }
     Site(**config).build()
 
