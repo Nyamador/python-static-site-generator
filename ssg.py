2650e396bbb48d56d43fe982fa287a99e0dd27ef
Nyamador <dselasi4u@gmail.com>
2020-08-16 00:37:21 +0000

    Fixed Import statement in ssg.py

--- ssg.py
+++ ssg.py
@@ -1,12 +1,11 @@
 import typer
-import Site from ssg.site
+from ssg.site import  Site
 
 def main(source="content", dest="dist"):
     config = {
         "source": source,
         "dest": dest
     }
-    site = Site(**config)
-    site.build()
+    Site(**config).build()
 
 typer.run(main)
